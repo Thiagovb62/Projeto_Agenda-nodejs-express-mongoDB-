8ccf798c56b28e17c73892387a1f97d550e2390d
thiagovb62@gmail.com <thiagovb62@gmail.com>
2022-07-26 08:43:29 -0300

    copiando a parte 5 e ajustando o contato.ejs

---
+++ Parte-6/routes.js
@@ -0,0 +1,24 @@
+const express = require("express"); 
+const routes = express.Router();
+const homeController = require("./src/controller/homeController");
+const loginController = require("./src/controller/loginController");
+const contactController = require("./src/controller/contactController");
+
+
+const {LoginRequired} = require("./src/middlewares/index");
+// Rotas da Home
+routes.get("/", homeController.paginaInicia); 
+
+// Rota da Login
+routes.get("/login/index", loginController.index);
+routes.post("/login/register", loginController.register);
+routes.post("/login/login", loginController.login);
+routes.get("/login/logout", loginController.logout);
+
+// Rota do Contato
+routes.get("/contato/index",LoginRequired,contactController.index);
+routes.post('/contato/register', LoginRequired,contactController.register);
+routes.get('/contato/index/:id', LoginRequired,contactController.editIndex)
+// Exportação das rotas
+
+module.exports = routes;
